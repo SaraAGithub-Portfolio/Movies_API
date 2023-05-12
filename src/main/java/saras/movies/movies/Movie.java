a24116bb127598540fe58afe466446f8b85a981a
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-05-12 16:30:33 +0100

    Added necessary constructors: @Data @AllArgsConstructor @NoArgsConstructor

--- src/main/java/saras/movies/movies/Movie.java
+++ src/main/java/saras/movies/movies/Movie.java
@@ -1,11 +1,18 @@
 package saras.movies.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 @Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
-
+    @Id
     private ObjectId id;
 
     private String imdbId;
